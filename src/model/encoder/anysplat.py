74f4910344848e77af997279cbf78b99d2626391
GiperHsion <xiongzhipeng@njust.edu.cn>
2026-06-01 16:43:58 +0800

    fixed bug of distill info scale

--- src/model/encoder/anysplat.py
+++ src/model/encoder/anysplat.py
@@ -1017,6 +1017,8 @@ class EncoderAnySplat(Encoder[EncoderAnySplatCfg]):
 
         infos = {}
         infos["scene_scale"] = scene_scale
+        if "scene_scale" not in distill_infos:
+            distill_infos["scene_scale"] = scene_scale
         # total_pixel_num = B * H * W * V
         first_batch_pixel_num = H * W * V
         # total_gs_num = voxelized_gs_num + supplement_gs_num
